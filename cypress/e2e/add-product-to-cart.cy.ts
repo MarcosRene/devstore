0020a26c07a44a04723ea7ce0c7d4d6318ba7bce
Marcos Rene <renemoura789@gmail.com>
2024-05-30 12:08:47 -0300

    test: fix query moleton to moletom

--- cypress/e2e/add-product-to-cart.cy.ts
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -24,7 +24,7 @@ describe('add product to cart', () => {
   })
 
   it('should be able to search for a product and add it to the cart', () => {
-    cy.searchByQuery('moleton')
+    cy.searchByQuery('moletom')
 
     cy.get('a[href^="/product"]').first().click()
 
--- cypress/e2e/search.cy.ts
+++ cypress/e2e/search.cy.ts
@@ -1,6 +1,6 @@
 describe('search products', () => {
   it('should be able to search for products', () => {
-    cy.searchByQuery('moleton')
+    cy.searchByQuery('moletom')
 
     cy.location('pathname').should('include', '/search')
     cy.location('search').should('include', 'q=moletom')
